51e1778ae0bc30668fd1a2c2302de6aa7015386d
Nikos Paraskevopoulos <nikospara@yahoo.com>
2016-01-06 18:53:22 +0200

    Let the template URL (what Angular knows) and template path (what RequireJS loads) be specified independent of each other

--- templateCache.js
+++ templateCache.js
@@ -3,15 +3,22 @@ define(['module', './currentModule'], function(module, currentModule) {
 	
 	/** RequireJS module loader entry point. */
 	function load(name, parentRequire, onload, config) {
-		parentRequire(['text!' + name], function(t) {
+		var index = name.indexOf('|'), templateUrl = name, templatePath = name;
+
+		if( index > 0 ) {
+			templatePath = name.substring(0,index);
+			templateUrl = name.substring(index+1) || templatePath;
+		}
+
+		parentRequire(['text!' + templatePath], function(t) {
 			if( !config || !config.isBuild ) {
 				currentModule.run(['$templateCache', function($templateCache) {
-					$templateCache.put(name, t);
+					$templateCache.put(templateUrl, t);
 				}]);
 			}
 			onload({
 				text: t,
-				path: name
+				path: templateUrl
 			});
 		});
 	}
